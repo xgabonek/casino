4bd58aa31374146fb00a0101520b5468583aa3fd
xgabonek <gabonminecraft@gmail.com>
2025-03-18 17:20:11 +0100

    Casino v1.0

--- casino.py
+++ casino.py
@@ -1,2 +1,55 @@
 import time
 import random
+import slot_machine as sm
+
+
+def roulette():
+    pass
+
+def slot_machine():
+    sm.main()
+def poker():
+    pass
+
+def blackjack():
+    pass
+
+def main():
+    while True:
+        print("----------------")
+        print("Witaj w kasynie!")
+        print("----------------")
+        print()
+        print("--------------------------------------------------------------------------------------")
+        print("Dostępne gry: \n1 - Ruletka \n2 - Slot Machine \n3 - Poker \n4 - Blackjack \n0 - Wyjdź")
+        print("--------------------------------------------------------------------------------------")
+
+        wybor = input("Wybierz grę (1-4): ")
+        if not wybor.isdigit():
+            print("Podaj liczbę.")
+            continue
+        
+        wybor = int(wybor)
+
+        if wybor > 4:
+            print("Nieprawidłowa liczba.")
+            continue
+        if wybor < 0:
+            print("Nieprawidłowa liczba")
+            continue
+        
+        match wybor:
+            case 1:
+                roulette()
+            case 2:
+                slot_machine()
+            case 3:
+                poker()
+            case 4:
+                blackjack()
+            case 0:
+                print("Wychodzę...")
+                time.sleep(1)
+                break
+if __name__ == '__main__':
+    main()
---
+++ slot_machine.py
@@ -0,0 +1,80 @@
+import random
+import time
+
+def spin_row():
+    symbole = ['🍒', '🍉', '🍎', '🍊', '⭐']
+
+    return [random.choice(symbole) for _ in range(3)]
+
+def print_row(row):
+    print("-----------")
+    print("|".join(row))
+    print("-----------")
+
+def get_wyplata(row, bet):
+    if row[0] == row[1] == row[2]:
+        if row[0] == '🍒':
+            return bet * 3
+        elif row[0] == '🍉':
+            return bet * 4
+        elif row[0] == '🍎':
+            return bet * 5
+        elif row[0] == '🍊':
+            return bet * 7
+        elif row[0] == '⭐':
+            return bet * 10
+    return 0
+
+def main():
+    balans = 100
+    print("--------------------------")
+    print("Witaj w slot machine!")
+    print("Symbole: 🍒 🍉 🍎 🍊 ⭐")
+    print("--------------------------")
+
+    while balans > 0:
+        print(f"Twój balans: {balans}zł.")
+
+        bet = input("Ile chcesz postawić: ")
+
+        if not bet.isdigit():
+            print("Proszę wpisać numer.")
+            continue
+       
+        bet = int(bet)
+
+        if bet > balans:
+            print("Nie możesz postawić tej kwoty.")
+            continue
+        
+        if bet <= 0:
+            print("Nie możesz postawić tej kwoty.")
+            continue
+        
+        balans -= bet
+
+        row = spin_row()
+        print("Kręcenie...\n")
+        time.sleep(1)
+        print_row(row)
+
+        wyplata = get_wyplata(row, bet)
+
+        if wyplata > 0:
+            print(f"Wygrałeś {wyplata}zł!")
+        else:
+            print("Przegrałeś!")
+
+        balans += wyplata
+
+        zagraj_ponownie = input("Czy chcesz zagrać ponownie? (T/N): ").upper()
+
+        if zagraj_ponownie == 'N':
+            break
+
+    print("-----------------------------------------------")
+    print(f"Zakończono! Twój końcowy balans to: {balans}. ")
+    print("-----------------------------------------------")
+
+if __name__ == '__main__':
+    main()
