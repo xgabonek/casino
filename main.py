8af1c8b9eb458e94333f0bc6a6c42384a4c8a584
drozd <drozdmisha2023@gmail.com>
2025-03-19 11:57:04 +0100

    Casino v2.0

--- main.py
+++ main.py
@@ -5,23 +5,38 @@ import slot_machine as sm
 import pygui
 
 class Casino:
-    def __init__(self, WIDTH, HEIGHT):
+    def __init__(self, WIDTH, HEIGHT, user_playing):
         pygame.init()
         pygame.font.init()
 
         self.WIDTH = WIDTH
         self.HEIGHT = HEIGHT
         self.FPS = 60   
-        self.background_color = self.hex_to_rgb("#22cc22")
+        self.background_color = self.hex_to_rgb("#a1eb34")
+
+        self.user_playing = user_playing
 
         self.surface = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         self.clock = pygame.time.Clock()
+
+        self.current_window = 0 # 0 - Main menu; 1 - Roulette; 2 - Slot machine; 3 - Poker; 4 - Blackjack
     
     def run(self):
         while True:
             self.handle_events()
             self.surface.fill(self.background_color) # bg color
-            self.main_menu()
+            match self.current_window:
+                case 0:
+                    self.main_menu()
+                case 1:
+                    self.roulette()
+                case 2:
+                    self.slot_machine()
+                case 3:
+                    self.poker()
+                case 4:
+                    self.blackjack()
+
             self.clock.tick(self.FPS) # updating every 1/60 seconds
             pygame.display.flip() # updating the display
 
@@ -34,7 +49,7 @@ class Casino:
         pass
 
     def slot_machine(self):
-        sm.main()
+        
 
     def poker(self):
         pass
@@ -43,15 +58,26 @@ class Casino:
         pass
 
     def main_menu(self):
-        button_slot_machine = pygui.Button(100, 100, 100, 100, 'Nigger', color="#ffffff")
+        button_slot_machine = pygui.Button(pos=(100, self.HEIGHT//2 - 50), 
+                                           width=200, height=100, text='Slot machine', background_color="#509c00", color="#333", 
+                                           font='Comic sans', font_size=30,
+                                           function=self.change_window, args=[2])
         button_slot_machine.draw(self.surface)
+
+    def change_window(self, window):
+        self.current_window = window
 
     def hex_to_rgb(self, value):
         value = value.lstrip('#')
         lv = len(value)
         return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
+class Account:
+    def __init__(self, username):
+        self.username = username
+        self.balance = 1000
 
 if __name__ == '__main__':
-    casino = Casino(800, 600)
+    player = Account('joumey')
+    casino = Casino(800, 600, player)
     casino.run()
--- pygui.py
+++ pygui.py
@@ -1,12 +1,12 @@
 import pygame
+import time
 
 class Button:
-    def __init__(self, x, y, width, height, text, background_color="#6c6", color="#fff", hover_background_color="#ccc", hover_color="#5b5", font='Arial', font_size=16):
+    def __init__(self, pos, width, height, text, background_color="#6c6", color="#fff", hover_background_color="#5b5", hover_color="#ccc", font='Arial', font_size=16, function=lambda _: ..., args=None, sleep=0.1):
         pygame.init()
         pygame.font.init()
         
-        self.x = x
-        self.y = y
+        self.x, self.y = pos
         self.width = width
         self.height = height
         self.text = text
@@ -17,7 +17,11 @@ class Button:
         self.font_size = font_size
         self.font = font
         self.font = pygame.font.SysFont(self.font, self.font_size)
+        self.text_size = self.font.size(self.text)
         self.text = self.font.render(self.text, False, self.hex_to_rgb(color))
+        self.function = function
+        self.args = args
+        self.sleep = sleep
 
     def check_events(self):
         mouse = pygame.mouse.get_pos()
@@ -30,11 +34,25 @@ class Button:
             'clicked': clicked[0]
             }
 
+    def process(self):
+        self.function(self.args)
+        time.sleep(self.sleep)
+
     def draw(self, surface):
-        pygame.draw.rect(surface, self.hex_to_rgb(self.background_color), (self.x, self.y, self.width, self.height))
-        surface.blit(self.text, (self.x, self.y))
+        events = self.check_events()
+
+        hover = events.get('hover')
+        if events.get('clicked'):
+            self.process()
+
+        color = self.hex_to_rgb(self.background_color) if not hover else self.hex_to_rgb(self.hover_background_color)
+
+        pygame.draw.rect(surface, color, (self.x, self.y, self.width, self.height))
+        surface.blit(self.text, (self.x + self.width // 2 -  self.text_size[0] // 2, self.y + self.height // 2 - self.text_size[1] // 2))
 
     def hex_to_rgb(self, value):
         value = value.lstrip('#')
+        if len(value) == 3:
+            value = ''.join([i * 2 for i in value])
         lv = len(value)
         return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
